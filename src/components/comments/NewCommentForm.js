2fcac7d056abca8819d6e0367445f6542a984052
Sarieh-M <sariehmuhammad@gmail.com>
2023-06-13 16:36:37 +0300

    React Qoutes project

---
+++ src/components/comments/NewCommentForm.js
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useParams} from 'react';
+
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import classes from './NewCommentForm.module.css';
+
+const NewCommentForm = (props) => {
+  const commentTextRef = useRef();
+
+  const {sendRequest, status, error} = useHttp(addComment);
+
+  const {onAddComment} = props;
+
+  useEffect(() => {
+    if(status === 'completed' && !error){
+      onAddComment();
+    }
+  })
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const enteredText = commentTextRef.current.value;
+    // optional: Could validate here
+
+    sendRequest({commentData: {text: enteredText}, quoteId: props.quoteId});
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitFormHandler}>
+      <div className={classes.control} onSubmit={submitFormHandler}>
+        <label htmlFor='comment'>Your Comment</label>
+        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+      </div>
+      <div className={classes.actions}>
+        <button className='btn'>Add Comment</button>
+      </div>
+    </form>
+  );
+};
+
+export default NewCommentForm;
